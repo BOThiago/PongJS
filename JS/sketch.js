763ef1d02ee8c341b613b13b802b1db4336eecc3
Thiago Barreto De Oliveira <91765675+BOThiago@users.noreply.github.com>
2023-01-10 22:54:03 -0300

    Implementação de colisão nas bordas.

--- JS/sketch.js
+++ JS/sketch.js
@@ -1,7 +1,9 @@
+
+//Variáveis da bolinha
 let xBolinha = 300;
 let yBolinha = 200;
 let diametro = 15;
-
+let raio = diametro / 2;
 let velocidadeXBolinha = 6;
 let velocidadeYBolinha = 6;
 
@@ -11,7 +13,25 @@ function setup() {
 
 function draw() {
     background(0);
-    circle(xBolinha, yBolinha, diametro);
-    xBolinha += velocidadeXBolinha;
-    yBolinha -= velocidadeYBolinha;
+    movimentaBolinha();
+    mostraBolinha();
+    verificaColisaoBorda();
 }
+
+function mostraBolinha() {
+    circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha() {
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoBorda() {
+    if (xBolinha + raio > width || xBolinha - raio < 0){
+        velocidadeXBolinha *= -1;
+    }
+    if (yBolinha + raio > height || yBolinha - raio < 0){
+        velocidadeYBolinha *= -1;
+    }
+} 
